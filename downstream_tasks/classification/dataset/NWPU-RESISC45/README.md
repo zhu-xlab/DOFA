ffff2521ee43bca2879a73d93d69e357b7b6cc22
root <xiongzhitong@gmail.com>
2024-04-10 13:05:34 +0000

    update

---
+++ downstream_tasks/classification/dataset/NWPU-RESISC45/README.md
@@ -0,0 +1,13 @@
+The split files are borrowed from scale-mae: https://github.com/bair-climate-initiative/scale-mae
+Please cite the original paper for the dataset: 
+"Remote Sensing Image Scene Classification: Benchmark and State of the Art"
+
+```python
+classes=['airplane', 'airport', 'baseball_diamond', 'basketball_court', 'beach', 'bridge',\
+            'chaparral', 'church', 'circular_farmland', 'cloud', 'commercial_area', 'dense_residential',\
+            'desert', 'forest', 'freeway', 'golf_course', 'ground_track_field', 'harbor', 'industrial_area',\
+            'intersection', 'island', 'lake', 'meadow', 'medium_residential', 'mobile_home_park', 'mountain',\
+            'overpass', 'palace', 'parking_lot', 'railway', 'railway_station', 'rectangular_farmland', 'river',\
+            'roundabout', 'runway', 'sea_ice', 'ship', 'snowberg', 'sparse_residential', 'stadium', 'storage_tank', \
+            'tennis_court', 'terrace', 'thermal_power_station', 'wetland'], 
+```
---
+++ downstream_tasks/classification/work_dirs/examplenet_8xb32_in1k/20240409_185853/vis_data/config.py
@@ -0,0 +1,179 @@
+auto_scale_lr = dict(base_batch_size=1024)
+custom_imports = dict(imports='models')
+data_preprocessor = dict(
+    mean=[
+        123.675,
+        116.28,
+        103.53,
+    ],
+    num_classes=45,
+    std=[
+        58.395,
+        57.12,
+        57.375,
+    ],
+    to_rgb=True)
+dataset_type = 'CustomDataset'
+default_hooks = dict(
+    checkpoint=dict(_scope_='mmpretrain', interval=1, type='CheckpointHook'),
+    logger=dict(_scope_='mmpretrain', interval=100, type='LoggerHook'),
+    param_scheduler=dict(_scope_='mmpretrain', type='ParamSchedulerHook'),
+    sampler_seed=dict(_scope_='mmpretrain', type='DistSamplerSeedHook'),
+    timer=dict(_scope_='mmpretrain', type='IterTimerHook'),
+    visualization=dict(
+        _scope_='mmpretrain', enable=False, type='VisualizationHook'))
+default_scope = 'mmpretrain'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+launcher = 'none'
+load_from = None
+log_level = 'INFO'
+model = dict(
+    backbone=dict(depth=18, type='ExampleNet'),
+    head=dict(
+        in_channels=512,
+        loss=dict(loss_weight=1.0, type='CrossEntropyLoss'),
+        num_classes=1000,
+        topk=(
+            1,
+            5,
+        ),
+        type='LinearClsHead'),
+    neck=dict(type='GlobalAveragePooling'),
+    type='ImageClassifier')
+optim_wrapper = dict(
+    optimizer=dict(betas=(
+        0.9,
+        0.999,
+    ), eps=1e-08, lr=0.000125, type='AdamW'),
+    weight_decay=0.05)
+param_scheduler = [
+    dict(
+        begin=0,
+        by_epoch=True,
+        convert_to_iter_based=True,
+        end=20,
+        type='LinearLR'),
+    dict(
+        T_max=280,
+        begin=20,
+        by_epoch=True,
+        end=300,
+        eta_min=1.25e-06,
+        type='CosineAnnealingLR'),
+]
+randomness = dict(deterministic=False, seed=None)
+resume = False
+test_cfg = dict()
+test_dataloader = dict(
+    batch_size=128,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        data_prefix='val',
+        data_root='../../data/imagenet',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                backend='pillow',
+                edge='short',
+                interpolation='bicubic',
+                scale=224,
+                type='ResizeEdge'),
+            dict(crop_size=224, type='CenterCrop'),
+            dict(type='PackInputs'),
+        ],
+        type='CustomDataset'),
+    num_workers=8,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = dict(
+    topk=(
+        1,
+        5,
+    ), type='Accuracy')
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        backend='pillow',
+        edge='short',
+        interpolation='bicubic',
+        scale=224,
+        type='ResizeEdge'),
+    dict(crop_size=224, type='CenterCrop'),
+    dict(type='PackInputs'),
+]
+train_cfg = dict(by_epoch=True, max_epochs=300, val_interval=1)
+train_dataloader = dict(
+    batch_size=128,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        ann_file='train-resisc.txt',
+        data_prefix='train-resisc',
+        data_root='./dataset/NWPU-RESISC45',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                backend='pillow',
+                interpolation='bicubic',
+                scale=224,
+                type='RandomResizedCrop'),
+            dict(direction='horizontal', prob=0.5, type='RandomFlip'),
+            dict(type='PackInputs'),
+        ],
+        type='CustomDataset',
+        with_label=True),
+    num_workers=8,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=True, type='DefaultSampler'))
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        backend='pillow',
+        interpolation='bicubic',
+        scale=224,
+        type='RandomResizedCrop'),
+    dict(direction='horizontal', prob=0.5, type='RandomFlip'),
+    dict(type='PackInputs'),
+]
+val_cfg = dict()
+val_dataloader = dict(
+    batch_size=128,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        data_prefix='val',
+        data_root='../../data/imagenet',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                backend='pillow',
+                edge='short',
+                interpolation='bicubic',
+                scale=224,
+                type='ResizeEdge'),
+            dict(crop_size=224, type='CenterCrop'),
+            dict(type='PackInputs'),
+        ],
+        type='CustomDataset'),
+    num_workers=8,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = dict(
+    topk=(
+        1,
+        5,
+    ), type='Accuracy')
+vis_backends = [
+    dict(_scope_='mmpretrain', type='LocalVisBackend'),
+]
+visualizer = dict(
+    _scope_='mmpretrain',
+    type='UniversalVisualizer',
+    vis_backends=[
+        dict(type='LocalVisBackend'),
+    ])
+work_dir = './work_dirs/examplenet_8xb32_in1k'
