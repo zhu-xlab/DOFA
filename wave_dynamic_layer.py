922266017cb21085e8b45e9620af3f5f26104625
root <xiongzhitong@gmail.com>
2024-04-10 12:54:55 +0000

    update

--- wave_dynamic_layer.py
+++ wave_dynamic_layer.py
@@ -4,11 +4,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 
-from util.pos_embed import get_1d_sincos_pos_embed_from_grid_torch
-
 random_seed = 1234
 torch.manual_seed(random_seed)
 
+
+def get_1d_sincos_pos_embed_from_grid_torch(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = torch.arange(embed_dim // 2, dtype=torch.float32, device=pos.device)
+    omega /= embed_dim / 2.0
+    omega = 1.0 / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = torch.einsum("m,d->md", pos, omega)  # (M, D/2), outer product
+
+    emb_sin = torch.sin(out)  # (M, D/2)
+    emb_cos = torch.cos(out)  # (M, D/2)
+
+    emb = torch.cat([emb_sin, emb_cos], dim=1)  # (M, D)
+    return emb
 
 class TransformerWeightGenerator(nn.Module):
     def __init__(self, input_dim, output_dim, embed_dim, num_heads=4, num_layers=1):
